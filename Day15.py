46f881a21855f4f67910554d57232e008d77a7bd
arorashivoy <85881270+arorashivoy@users.noreply.github.com>
2021-12-15 21:51:23 +0530

    Day 15 (Part 1)

---
+++ Day15.py
@@ -0,0 +1,105 @@
+# Advent Of coding 
+# Day 15
+#
+# Using Dijkstra's algorithm
+# By Shivoy Arora
+
+""" Add risk value of adjacent vertices """
+def AddAdj(vertex, risk):
+    global notSptSet, dataset
+    global readings
+
+    i = vertex[0]
+    j = vertex[1]
+
+    try:
+        if dataset[i+1][j] != "X" and dataset[i+1][j] < (risk + int(readings[i+1][j])):
+            pass
+        else:
+            # string the min risk to get to that vertex in dataset
+            dataset[i+1][j] = risk + int(readings[i+1][j])
+            # add that vertex in the set which contains vertices whose risk is know but is not in sptSet
+            notSptSet.add((i+1,j))
+    except IndexError:
+        pass
+    try:
+        if i > 0:
+            if dataset[i-1][j] != "X" and dataset[i-1][j] < (risk + int(readings[i-1][j])):
+                pass
+            else:
+                dataset[i-1][j] = risk + int(readings[i-1][j])
+                notSptSet.add((i-1, j))
+    except IndexError:
+        pass
+    try:
+        if dataset[i][j+1] != "X" and dataset[i][j+1] < (risk + int(readings[i][j+1])):
+            pass
+        else:
+            dataset[i][j+1] = risk + int(readings[i][j+1])
+            notSptSet.add((i, j+1))
+    except IndexError:
+        pass
+    try:
+        if j > 0:
+            if dataset[i][j-1] != "X" and dataset[i][j-1] < (risk + int(readings[i][j-1])):
+                pass
+            else:
+                dataset[i][j-1] = risk + int(readings[i][j-1])
+                notSptSet.add((i,j-1))
+    except IndexError:
+        pass
+
+""" Add vertex to sptSet with min risk """
+def MinRisk():
+    global sptSet
+    global notSptSet, dataset
+
+    minRisk = None
+    minVer = (0,0)
+
+    # checking notSptSet for the next vertex with least risk
+    for i in notSptSet:
+        if not minRisk or minRisk > dataset[i[0]][i[1]]:
+            minRisk = dataset[i[0]][i[1]]
+            minVer = (i[0], i[1])
+
+    # removing the vertex from notSptSet which is added to sptSet
+    notSptSet.remove(minVer)
+    return minVer
+
+""" Main Function """
+# file = open("input/input15.txt","r")
+file = open("input/test.txt","r")
+readings = [i.strip() for i in file.readlines()]
+
+sptSet = [(0,0),]
+notSptSet = set()
+
+dataset = [["X" for _ in range(len(readings[0]))] for _ in range(len(readings))]
+dataset[0][0] = 0
+
+risk = 0
+
+run = True
+while run:
+    AddAdj(sptSet[-1], risk)
+    minVertex = MinRisk()
+    sptSet.append(minVertex)
+    risk = dataset[minVertex[0]][minVertex[1]]
+
+    # if the element added to sptSet is the destination then exiting
+    if sptSet[-1] == ((len(readings) - 1), (len(readings[-1]) - 1)):
+        run = False
+
+print("Part 1")
+print("Min Risk:", risk)
+
+print()
+
+# Part 2
+tempReadings = [[j for j in i] for i in readings]
+
+for i in range(len(readings)):
+    for j in range(len(readings[i])):
+        if readings[i][j] != "9":
+            pass
